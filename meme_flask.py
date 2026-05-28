78585c3317d98e51e5ac3d54f2d24e0a2608b9bf
bhavya-rajput1412 <bhavya.rajput1412@gmail.com>
2026-05-28 16:53:08 +0530

    fixed port config

--- meme_flask.py
+++ meme_flask.py
@@ -31,6 +31,6 @@ def index():
     return render_template("index.html", memes=memes)
 
 
-port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)  
